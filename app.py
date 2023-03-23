177355096ea7c330b3cb8f6b9cffa8b258d25361
marlontamo <marlontamo@gmail.com>
2023-03-23 23:40:41 +0800

    added gallery page

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 def hello():
     return render_template("home.html",title ="Homepage")
 
-
 @app.route("/about")
 def about():
     return render_template("about.html",title= "About")
@@ -15,3 +14,7 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html",title="Contact")
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html",title="Gallery")
