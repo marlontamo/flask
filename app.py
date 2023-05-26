ca53922f27b2eb9cbf85d17df31cb874d1801a5c
Marlon Tamo <marlontamo@gmail.com>
2023-05-26 13:34:05 +0800

    added spaces

--- app.py
+++ app.py
@@ -4,17 +4,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
+
     return render_template("home.html",title ="Homepage")
 
 @app.route("/about")
 def about():
+
     return render_template("about.html",title= "About")
 
 
 @app.route("/contact")
 def contact():
+
     return render_template("contact.html",title="Contact")
 
 @app.route("/gallery")
 def gallery():
+    
     return render_template("gallery.html",title="Gallery")
